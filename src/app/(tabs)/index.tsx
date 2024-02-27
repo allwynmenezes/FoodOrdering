704997c4862fe69801a2e8e242bba0dad097eacf
allwyn menezes <allwynsudeep@gmail.com>
2024-02-26 22:07:11 -0500

    Revert "Move product and product details into menu folder and update navigation"
    This reverts commit 1bf8bb89cf8a2a0d547f7acae591a2e168e09081.
    
    Revert 'Move product and product details into menu folder and update navigation' commit to add comments

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,15 @@
-import { Redirect } from "expo-router";
+import { View, FlatList } from "react-native";
+import ProductListItem from "@components/ProductListItem";
+import products from "@assets/data/products";
 
-export default function index() {
-  return <Redirect href={"/(tabs)/menu/"} />;
+export default function MenuScreen() {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,6 @@
-import { StyleSheet, Image, Text, View, Pressable } from "react-native";
+import { StyleSheet, Image, Text, View } from "react-native";
 import Colors from "@/constants/Colors";
-import { PizzaSize, Product } from "@/types";
-import { Link } from "expo-router";
+import { Product } from "@/types";
 
 export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -10,28 +9,17 @@ type ProductListItemProps = {
   product: Product;
 };
 
-//product can also be directly associated with the type Product in the types file
-//here we are using a object variable which contains various types to assign to product
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    //asChild attribute specifies to inherit styles defined on the child
-    //with asChild the child element should have a onPress attribute
-    //(where it is allowed, example Text element) or it should be a Pressable element
-
-    //backticks is used to represent dynamic string which also uses ${} to specify the dynamic value
-    <Link href={`menu/${product.id}`} asChild>
-      <Pressable style={styles.container}>
-        <Image
-          //default value is needed because image type is defined to be either link or NULL
-          source={{ uri: product.image || defaultPizzaImage }}
-          style={styles.image}
-          //this is to maintain uniform image size
-          resizeMode="contain"
-        />
-        <Text style={styles.title}>{product.name}</Text>
-        <Text style={styles.price}>{product.price}</Text>
-      </Pressable>
-    </Link>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price}</Text>
+    </View>
   );
 };
 
